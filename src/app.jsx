eaa5063a2f44b9a723e94f48c1722e3060a6b46a
JayeHa <66169832+JayeHa@users.noreply.github.com>
2021-02-25 18:32:39 +0900

    4.10 하는중..어렵당..

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
 import Habits from './components/habits';
 
-function App() {
-  return <Habits />;
+class App extends Component {
+  state = {
+    habits: [
+        { id: 1, name: 'Reading', count:0 },
+        { id: 2, name: 'Running', count:0 },
+        { id: 3, name: 'Coding', count:0 },
+    ],
+};
+
+  render() {
+    return(
+      <>
+      <nav>Habit Tracker<span>{this.state.habits.length}</span></nav>
+      <div className="addHabit">
+        <input type="text" name="" id="" placeholder="Habit"/>
+        <button className="btn_addHabit">Add</button>
+      </div>
+      <Habits />
+      <button className="resetAll">Reset All</button>
+      </>
+    );
+
+  } 
 }
 
 export default App;
